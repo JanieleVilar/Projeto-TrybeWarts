b2a483bcb05ba55042116cfcb8d01be861161ffe
Vinicius Gomes <viniciusemog@hotmail.com>
2022-01-11 16:45:25 -0300

    Resoluçao do requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+const loginButton = document.getElementById('login-form-button');
+const inputEmail = document.getElementById('input-email');
+const inputPassword = document.getElementById('input-password');
+
+// Req 3
+function loginResponse() {
+  if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+    alert ('Olá, Tryber!');
+  } else {
+    alert ('Email ou senha inválidos.');
+  }
+}
+loginButton.addEventListener('click', loginResponse);

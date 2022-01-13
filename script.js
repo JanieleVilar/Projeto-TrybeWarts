77a9223d4b2ad85a0c8f2586e24eb4fb114b8476
Vinicius Gomes <viniciusemog@hotmail.com>
2022-01-13 18:42:58 -0300

    Alteraçoes antes do pull

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const loginButton = document.getElementById('login-form-button');
 const inputEmail = document.getElementById('input-email-header');
 const inputPassword = document.getElementById('input-password');
+const textarea = document.getElementById('textarea');
+const text = document.getElementById('counter');
+const submitButton = document.getElementById('submit-btn');
 
 // Req 3
 function loginResponse() {
@@ -11,3 +14,14 @@ function loginResponse() {
   }
 }
 loginButton.addEventListener('click', loginResponse);
+
+// Req 21
+function returnCount() {
+  text.innerHTML = `${500 - this.value.length}`;
+}
+textarea.addEventListener('input', returnCount);
+
+function onload() {
+  text.innerHTML = '500';
+}
+onload();
